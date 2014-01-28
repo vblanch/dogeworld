a69d017071a315c8e98300144cfd784c7f18dda7
vblanch <victor@vblanch.com>
2014-01-28 02:02:28 +0100

    Adding obstacles. Removed button class.

--- game.js
+++ game.js
@@ -1,4 +1,7 @@
-var stage, player;
+var		PLAYER_IMAGE = 'assets/img/doge.png',
+		PLATFORM_IMAGE = 'assets/img/obstacle.png';
+
+//var stage, player;
 
 var shootHeld;                      //is the user holding a shoot command
 var lfHeld;                         //is the user holding a turn left command
@@ -6,7 +9,7 @@ var rtHeld;                         //is the user holding a turn right command
 var upHeld;                         //is the user holding an up command
 var downHeld;                         //is the user holding a down command
 
-var alive;							//is player alive?
+//var alive;							//is player alive?
 
 var WALK_FACTOR = 100;               //how far the player walks per frame
 
@@ -21,20 +24,228 @@ var KEYCODE_A = 65;                        //usefull keycode
 var KEYCODE_D = 68;                        //usefull keycode
 var KEYCODE_S = 83;                        //usefull keycode
 
-function init() {
+function _game() {
 	// code here.
 	
+	window.Game = this;
 	//player = new Player();
 		
-	alive = true;	//start alive
-	stage = new createjs.Stage("demoCanvas");
+	var self = this,
+		ticks = 0,
+		canvas,
+		stage,
+		world,
+		player,
+		/*w = getWidth(),
+		h = getHeight(),
+		*/
+		w = 640,
+		h = 480,
+		assets = [],
+		keyDown = false;
+		
+	//alive = true;	//start alive
+	//stage = new createjs.Stage("demoCanvas");
+		
+	// holds all collideable objects
+	var collideables = [];
+	this.getCollideables = function() { return collideables; };
+
+	// starts to load all the assets
+	this.preloadResources = function() {
+		self.loadImage(PLAYER_IMAGE);
+		self.loadImage(PLATFORM_IMAGE);
+	}
+
+	var requestedAssets = 0,
+		loadedAssets = 0;
+	// loads the assets and keeps track 
+	// of how many assets where there to
+	// be loaded
+	this.loadImage = function(e) {
+		var img = new Image();
+		img.onload = self.onLoadedAsset;
+		img.src = e;
+
+		assets[e] = img;
+
+		++requestedAssets;
+	}
+
+	// each time an asset is loaded
+	// check if all assets are complete
+	// and initialize the game, if so
+	this.onLoadedAsset = function(e) {
+		++loadedAssets;
+		if ( loadedAssets == requestedAssets ) {
+			self.initializeGame();
+		}
+	}
 	
-	preload();	//preload creates the player at the end
+
+	this.initializeGame = function() {
+		// creating the canvas-element
+		
+		canvas = document.createElement('canvas');
+		canvas.width = w;
+		canvas.height = h;
+		canvas.setAttribute("class", "demoCanvas"); 
+		document.body.appendChild(canvas);
+		
+		// initializing the stage
+		//stage = new Stage(canvas);
+		stage = new createjs.Stage(canvas); 
+		world = new createjs.Container();
+		stage.addChild(world);
+
+		// creating the Hero, and assign an image
+		// also position the hero in the middle of the screen
+		player = new Player(assets[PLAYER_IMAGE]);
+		player.x = w/2
+		player.y = h/2;
+		world.addChild(player);
+
+		// add a platform for the hero to collide with
+		self.addPlatform(w/2 - assets[PLATFORM_IMAGE].width/2, h/1.25);
+
+		// Setting the listeners
+		if ('ontouchstart' in document.documentElement) {
+			canvas.addEventListener('touchstart', function(e) {
+				self.handleKeyDown();
+			}, false);
+
+			canvas.addEventListener('touchend', function(e) {
+				self.handleKeyUp();
+			}, false);
+		} else {
+			document.onkeydown = self.handleKeyDown;
+			document.onkeyup = self.handleKeyUp;
+			document.onmousedown = self.handleKeyDown;
+			document.onmouseup = self.handleKeyUp;
+		}
+		
+		createjs.Ticker.setFPS(30);
+		//createjs.Ticker.addListener(self.tick);
+		createjs.Ticker.on("tick", self.tick);
+	}
+
+	this.tick = function(e)
+	{
+		ticks++;
+		player.tick();
+		stage.update();
+	}
 	
-	//demo button
-	var btn1 = stage.addChild(new Button("Hello world!", "#f00"));
-	btn1.x = 20;
-	btn1.y = 20;
+	// this method adds a platform at the
+	// given x- and y-coordinates and adds
+	// it to the collideables-array
+	this.addPlatform = function(x,y) {
+		x = Math.round(x);
+		y = Math.round(y);
+
+		var platform = new createjs.Bitmap(assets[PLATFORM_IMAGE]);
+		platform.x = x;
+		platform.y = y;
+		platform.snapToPixel = true;
+
+		world.addChild(platform);
+		collideables.push(platform);
+	}
+
+	/*
+	this.handleKeyDown = function(e)
+	{
+		if ( !keyDown ) {
+			keyDown = true;
+			player.jump();
+		}
+	}
+	*/
+	
+	this.processKeys = function(){
+		//alert("processing...1");
+		//handle turning 
+		if(lfHeld){
+			player.moveLeft();
+		} else if(rtHeld) {
+			player.moveRight(); // += event.delta/1000*WALK_FACTOR;
+		}
+		
+		if(!lfHeld && !rtHeld){
+			//player.stopHorizontal();
+		}
+		 
+		if(upHeld){
+			//player.y -= event.delta/1000*WALK_FACTOR;
+			player.moveUp();
+		} else if(downHeld) {
+			player.moveDown();
+			//player.y += event.delta/1000*WALK_FACTOR;
+		}		
+		
+		if(!upHeld && !downHeld){
+			//player.stopVertical();
+		}		
+	
+		//alert("processing...2");
+	}	
+	
+	//allow for WASD and arrow control scheme
+	this.handleKeyDown = function(e) {
+			//cross browser issues exist 
+			//alert("handling keys");
+			if(!e){ var e = window.event; }
+			switch(e.keyCode) {
+					case KEYCODE_SPACE:    shootHeld = true; break;;
+					case KEYCODE_A:
+					case KEYCODE_LEFT:     lfHeld = true; break;
+					case KEYCODE_D:
+					case KEYCODE_RIGHT:    rtHeld = true; break;
+					case KEYCODE_W:
+					case KEYCODE_UP:       upHeld = true; break;
+					case KEYCODE_S: 
+					case KEYCODE_DOWN:     downHeld = true; break;					
+					//case KEYCODE_ENTER:    if(canvas.onclick == handleClick){ handleClick(); }break;
+			}
+			//alert("test 2");
+			self.processKeys(); 
+			return false;
+	}	
+
+
+	
+	/*
+	this.handleKeyUp = function(e)
+	{
+		keyDown = false;
+	}
+	*/
+	
+	this.handleKeyUp = function(e) {
+			//cross browser issues exist 
+			if(!e){ var e = window.event; }
+			switch(e.keyCode) {
+					case KEYCODE_SPACE:    shootHeld = false; break;
+					case KEYCODE_A:
+					case KEYCODE_LEFT:     lfHeld = false; break;
+					case KEYCODE_D:
+					case KEYCODE_RIGHT:    rtHeld = false; break;
+					case KEYCODE_W:
+					case KEYCODE_UP:       upHeld = false; break;
+					case KEYCODE_S:
+					case KEYCODE_DOWN:     downHeld = false; break;							
+			}
+			self.processKeys(); 
+			return false;
+	}	
+
+	self.preloadResources();
+};
+
+new _game();	
+
+
+/*	
 		
 	//register key functions
 	document.onkeydown = handleKeyDown;
@@ -74,59 +285,65 @@ function init() {
 			}
 	}
 
-	
-}
-
-function tick(event) { 
-	//console.log("TICK!!!"); 
-	// move 100 pixels per second (elapsedTimeInMS / 1000msPerSecond * pixelsPerSecond):
-	//player.x += event.delta/1000*100;
-	// this will log a steadily increasing value:
-			
-	//player pos
-	
-	//handle turning 
-	if(alive && lfHeld){
-			player.x -= event.delta/1000*WALK_FACTOR;
-	} else if(alive && rtHeld) {
-			player.x += event.delta/1000*WALK_FACTOR;
-	}		
-	
-	if(alive && upHeld){
-			player.y -= event.delta/1000*WALK_FACTOR;
-	} else if(alive && downHeld) {
-			player.y += event.delta/1000*WALK_FACTOR;
-	}	
-	
-	//infinite stage
-	if(player.x>stage.canvas.width){player.x = 0};
-	if(player.x<0){player.x = stage.canvas.width};
-
-	if(player.y>stage.canvas.height){player.y = 0};
-	if(player.y<0){player.y = stage.canvas.height};				
-			
-	stage.update(event);	//stage update on every tick
-	//console.log("total time: "+createjs.Ticker.getTime());		
-}
-
-function preload(){
-	//preload images, etc
-	var img = new Image();
-	img.onload = onImageLoaded;
-	img.src = 'assets/img/doge.png';
-
-	function onImageLoaded(e) {
-		player = new Player(img);
-		stage.addChild(player);
-		player.reset();
-		player.setPosition(150, 100);
+	function tick(event) { 
+		//console.log("TICK!!!"); 
+		// move 100 pixels per second (elapsedTimeInMS / 1000msPerSecond * pixelsPerSecond):
+		//player.x += event.delta/1000*100;
+		// this will log a steadily increasing value:
+				
+		//player pos
 		
-		//create the ticker	
-		createjs.Ticker.setFPS(30);
-		createjs.Ticker.on("tick", tick);
-	}
-}
+		//handle turning 
+		if(alive && lfHeld){
+				player.x -= event.delta/1000*WALK_FACTOR;
+		} else if(alive && rtHeld) {
+				player.x += event.delta/1000*WALK_FACTOR;
+		}		
+		
+		if(alive && upHeld){
+				player.y -= event.delta/1000*WALK_FACTOR;
+		} else if(alive && downHeld) {
+				player.y += event.delta/1000*WALK_FACTOR;
+		}	
+		
+		//infinite stage
+		if(player.x>stage.canvas.width){player.x = 0};
+		if(player.x<0){player.x = stage.canvas.width};
 
+		if(player.y>stage.canvas.height){player.y = 0};
+		if(player.y<0){player.y = stage.canvas.height};				
+				
+		stage.update(event);	//stage update on every tick
+		//console.log("total time: "+createjs.Ticker.getTime());		
+	}
+
+	/*
+	function preload(){
+		//preload images, etc
+		var img = new Image();
+		img.onload = onImageLoaded;
+		img.src = 'assets/img/doge.png';
+
+		function onImageLoaded(e) {
+			player = new Player(img);
+			stage.addChild(player);
+			player.reset();
+			player.setPosition(150, 100);
+			
+			//create the ticker	
+			createjs.Ticker.setFPS(30);
+			createjs.Ticker.on("tick", tick);
+		}
+	}
+	*/
+
+	//self.preloadResources(); 
+//}
+
+
+
+
+//new _game();
 
 // notes
 //-------
--- player.js
+++ player.js
@@ -2,6 +2,7 @@
 	function Player(image) {
 		this.initialize(image);
 	}
+	
  	Player.prototype = new createjs.Bitmap();
 
  	// save the original initialize-method so it won't be gone after
@@ -10,21 +11,158 @@
 
  	// initialize the object
  	Player.prototype.initialize = function (image) {
+	    this.velocity = {x:0,y:0};
  		this.Bitmap_initialize(image);
  		this.name = 'Player';
  		this.snapToPixel = true;
+		this.PLAYER_MOV = 5;
  	}
 
  	// we will call this function every frame to 
-	Player.prototype.tick = function () {
-		this.y += 1;
- 	}
+	//Player.prototype.tick = function () {
+		/*this.y += 1;*/
+		//movement here
+ 	//}
+    Player.prototype.tick = function () {
+        //this.velocity.y += 1;
+		
+		//alert("contents of velocity:" + this.velocity.x + " " + this.velocity.y);
+		
+        // preparing the variables
+		
+		//alert("vely="+this.velocity.y);
+		
+		var c = 0,
+			cc = 0,
+			addY = this.velocity.y,
+			addX = this.velocity.x,	//new
+			bounds = getBounds(this),
+			cbounds,
+			collision = null,
+			collideables = Game.getCollideables();			
+			
+		cc=0;
+		//var addY = this.velocity.y;
+				
+		// for each collideable object we will calculate the
+		// bounding-rectangle and then check for an intersection
+		// of the hero's future position's bounding-rectangle
+		while ( !collision && cc < collideables.length ) {
+			cbounds = getBounds(collideables[cc]);
+			if ( collideables[cc].isVisible ) {
+				collision = calculateIntersection(bounds, cbounds, 0, addY);
+			}
+			if ( !collision && collideables[cc].isVisible ) {
+				// if there was NO collision detected, but somehow
+				// the hero got onto the "other side" of an object (high velocity e.g.),
+				// then we will detect this here, and adjust the velocity according to
+				// it to prevent the Hero from "ghosting" through objects
+				// try messing with the 'this.velocity = {x:0,y:25};'
+				// -> it should still collide even with very high values
+				if ( ( bounds.y < cbounds.y && bounds.y + addY > cbounds.y )
+				  || ( bounds.y > cbounds.y && bounds.y + addY < cbounds.y ) ) {
+					addY = cbounds.y - bounds.y;
+				} else {
+					cc++;
+				}
+			}
+		}
 
+		// if no collision was to be found, just
+		//  move the hero to it's new position
+		if ( !collision ) {
+			//if(this.velocity.y!=0) alert("no collision:"+this.y + " addy="+ addY);
+			this.y += addY;
+		// else move the hero as far as possible
+		// and then make it stop and tell the
+		// game, that the hero is now "an the ground"
+		} else {
+			//if(this.velocity.y!=0) alert("collision:"+this.y + " addy="+ addY);
+			this.y += addY - collision.height;
+			this.velocity.y = 0;
+		}
+		
+		//same for X!
+		/////////////
+		
+		cc=0;
+		while ( !collision && cc < collideables.length ) {
+			cbounds = getBounds(collideables[cc]);
+			if ( collideables[cc].isVisible ) {
+				collision = calculateIntersection(bounds, cbounds, addX, 0);
+			}
+			if ( !collision && collideables[cc].isVisible ) {
+				// if there was NO collision detected, but somehow
+				// the hero got onto the "other side" of an object (high velocity e.g.),
+				// then we will detect this here, and adjust the velocity according to
+				// it to prevent the Hero from "ghosting" through objects
+				// try messing with the 'this.velocity = {x:0,y:25};'
+				// -> it should still collide even with very high values
+				if ( ( bounds.x < cbounds.x && bounds.x + addX > cbounds.x )
+				  || ( bounds.x > cbounds.x && bounds.x + addX < cbounds.x ) ) {
+					addX = cbounds.x - bounds.x;
+				} else {
+					cc++;
+				}
+			}
+		}
+
+		// if no collision was to be found, just
+		//  move the hero to it's new position
+		if ( !collision ) {
+			//if(this.velocity.y!=0) alert("no collision:"+this.y + " addy="+ addY);
+			this.x += addX;
+		// else move the hero as far as possible
+		// and then make it stop and tell the
+		// game, that the hero is now "an the ground"
+		} else {
+			//if(this.velocity.y!=0) alert("collision:"+this.y + " addy="+ addY);
+			this.x += addX - collision.width;
+			this.velocity.x = 0;
+		}
+		
+		//stop movement
+		this.stopHorizontal();
+		this.stopVertical();
+		
+		console.log("x speed="+this.velocity.x);
+		console.log("y speed="+this.velocity.y);
+    }	
+
+	Player.prototype.moveLeft = function(){
+		this.velocity.x = -this.PLAYER_MOV;
+		//alert("moving left");
+	}
+	
+	Player.prototype.moveRight = function(){
+		this.velocity.x = this.PLAYER_MOV;
+	}	
+	
+	Player.prototype.moveUp = function(){
+		this.velocity.y = -this.PLAYER_MOV;
+	}
+	
+	Player.prototype.moveDown = function(){
+		this.velocity.y = this.PLAYER_MOV;
+	}
+	
+	Player.prototype.stopHorizontal = function(){
+		if(this.velocity.x>0) this.velocity.x -= 1; 
+		if(this.velocity.x<0) this.velocity.x += 1; 
+	}
+
+	Player.prototype.stopVertical = function(){
+		if(this.velocity.y>0) this.velocity.y -= 1;
+		if(this.velocity.y<0) this.velocity.y += 1;
+	}
+	
  	// this will reset the position of the Player
  	// we can call this e.g. whenever a key is pressed
- 	Player.prototype.reset = function() {
+ 	/*
+	Player.prototype.reset = function() {
  		this.y = 0;
  	}
+	*/
 	
  	Player.prototype.setPosition = function(x,y) {
  		this.x = x;
